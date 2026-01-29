57ebbc331d70002ce849582f24592e7915fd5566
omer.levy <omerlevy03@gmail.com>
2026-01-29 21:02:54 +0200

    2026-01-29_21-02

--- shell/prompt.sh
+++ shell/prompt.sh
@@ -1,3 +1,3 @@
 if command -v oh-my-posh >/dev/null 2>&1; then
-  eval "$(oh-my-posh init bash --config ~/.config/ohmyposh/gruvbox.omp.json)"
+  eval "$(oh-my-posh init bash --config ~/.config/ohmyposh/omer_custom.omp.json)"
 fi
