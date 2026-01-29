6b6de6988076f6493e012c4872756c7506f4a636
omer.levy <omerlevy03@gmail.com>
2026-01-30 00:59:41 +0200

    just orgenizing things

--- shell/prompt.sh
+++ shell/prompt.sh
@@ -1,3 +1,3 @@
 if command -v oh-my-posh >/dev/null 2>&1; then
-  eval "$(oh-my-posh init bash --config ~/.config/ohmyposh/custom.opm.json)"
+  eval "$(oh-my-posh init bash --config ~/.config/shell_prompt/minimal_gruvbox.opm.json)"
 fi
