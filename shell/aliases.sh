81e4681b5db9ac75c9113026722f4e6456a8eafa
omer.levy <omerlevy03@gmail.com>
2026-02-04 18:20:32 +0200

    added alias sleep

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -7,6 +7,8 @@ alias py='python3'
 alias v='nvim'
 alias gccobj='gcc -c'
 
+alias sleepnow='systemctl suspend'
+
 alias jarvis='ollama run qwen2.5-coder:7b'
 
 alias gitdate='git add . && git commit -m "$(date +%Y-%m-%d_%H-%M)" && git push'
