17c8896a03715f2e338d6e167aaa3d9bf0b1abeb
omer.levy <omerlevy03@gmail.com>
2026-02-17 22:13:46 +0200

    2026-02-17_22-13

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,14 +1,28 @@
+# bash
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 alias ls='ls --color=auto'
 
+# programming
 alias py='python3'
-alias v='nvim'
 alias gccobj='gcc -c'
 
-alias sleepnow='systemctl suspend'
+#screen
+alias m_office="~/.config/screenlayout/office_monitor_layout.sh"
+alias m_home="~/.config/screenlayout/home_monitor_layout.sh"
+alias m_laptop="~/.config/screenlayout/laptop_only_layout.sh"
 
+# TUIs
+alias v='nvim'
+alias bt='bluetui'
+alias wifi='nmuti'
+
+# systems
+alias sleepnow='systemctl suspend'
 alias jarvis='ollama run qwen2.5-coder:7b'
 
+# git
+alias gitadd='git add . & git status'
 alias gitdate='git add . && git commit -m "$(date +%Y-%m-%d_%H-%M)" && git push'
+alias gitlog='git log --graph --oneline --decorate'
