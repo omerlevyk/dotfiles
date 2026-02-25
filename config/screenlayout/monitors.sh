a87b08efc583bb89471ab97f1b0460a8b5fd3244
omer.levy <omerlevy03@gmail.com>
2026-02-25 13:28:41 +0200

    tmux paste

--- config/screenlayout/monitors.sh
+++ config/screenlayout/monitors.sh
@@ -5,6 +5,7 @@ BASE="$HOME/.config/screenlayout"
 # if home monitor
 if xrandr | grep "DP-1-1 connected" >/dev/null; then
   ~/.config/screenlayout/home_monitor_layout.sh
+  ~/.config/polybar/polybar_lanch.sh
 else # laptop only
   ~/.config/screenlayout/laptop_only_layout.sh
 fi
